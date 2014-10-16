ea3701cc7d8c9e66cff41b7a0736159f5da4df18
unknown <A00778394@SE12L0319-11.ad.bcit.ca>
2014-10-16 14:49:33 -0700

    Adding new version of program

---
+++ IVGDb/IVGDb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IVGDb.Startup))]
+namespace IVGDb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
