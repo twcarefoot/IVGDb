526974f7b66f364886f9f220137165254595908d
Kevin McKeen <kevmckeenis@outlook.com>
2014-11-17 08:55:06 -0800

    Pre-merge push

---
+++ IVGDb/IVGDb/Models/GameConsoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IVGDb.Models
+{
+    public class GameConsoleViewModel : VideoGame
+    {
+        public static List<Console> ChosenConsoles { get; set; }
+
+        public static List<Console> GetAllConsoles()
+        {
+            using(ivgdb_Entities db = new ivgdb_Entities())
+            {
+                return db.Consoles.ToList();
+            }
+        }
+    }
+}
---
+++ IVGDb/IVGDb/Models/UserModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IVGDb.Models
+{
+    public class UserModel : User
+    {
+        public string ConfirmPassword { get; set; }
+
+        //private static ivgdb_Entities db = new ivgdb_Entities();
+
+        public static User GetUser(string username)
+        {
+            using (ivgdb_Entities db = new ivgdb_Entities())
+            {
+                return db.Users.Where(p => p.Username == username).First();
+            }
+        }
+
+        public static string GetCurrentPassword(string username)
+        {
+            using (ivgdb_Entities db = new ivgdb_Entities())
+            {
+                return db.Users.Where(p => p.Username == username).Select(p => p.Password).ToString();
+            }
+        }
+
+        public static bool MatchingPasswords(string username, string newPassword)
+        {
+            using (ivgdb_Entities db = new ivgdb_Entities())
+            {
+                return db.Users.Where(p => p.Username == username).Select(p => p.Password).Equals(newPassword);
+            }
+        }
+
+        public static void Register(UserModel user)
+        {
+            using (ivgdb_Entities db = new ivgdb_Entities())
+            {
+                User newUser = new User();
+                newUser.Username = user.Username;
+                newUser.Email = user.Email;
+                newUser.Password = user.Password;
+                newUser.UserProfilePicLink = null;
+                db.Users.Add(newUser);
+                db.SaveChanges();
+            }
+        }
+    }
+}
